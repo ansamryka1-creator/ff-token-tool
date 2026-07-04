e71e4769573e0f95e46083c07afd5d52972e1dc4
ansamryka1-creator <ansamryka1@gmail.com>
2026-07-04 12:47:19 +0100

    Update ToastProvider.tsx

--- frontend/components/providers/ToastProvider.tsx
+++ frontend/components/providers/ToastProvider.tsx
@@ -1,19 +1,28 @@
 'use client'
 
+import { ReactNode } from 'react'
 import { Toaster } from 'sonner'
 
-export function ToastProvider() {
+interface ToastProviderProps {
+  children: ReactNode
+}
+
+export function ToastProvider({ children }: ToastProviderProps) {
   return (
-    <Toaster
-      position="bottom-right"
-      theme="dark"
-      toastOptions={{
-        style: {
-          background: '#0f1629',
-          border: '1px solid #1a2a4a',
-          color: '#fff',
-        },
-      }}
-    />
+    <>
+      {children}
+
+      <Toaster
+        position="bottom-right"
+        theme="dark"
+        toastOptions={{
+          style: {
+            background: '#0f1629',
+            border: '1px solid #1a2a4a',
+            color: '#fff',
+          },
+        }}
+      />
+    </>
   )
 }
